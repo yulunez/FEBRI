98dd417fe7f9ed0df8050f1a8000cca44463b281
yulunez <yaru75665@gmail.com>
2025-05-16 10:26:11 -0500

    Creacion del front

---
+++ imagenes/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menuLateral = document.querySelector('.menu-lateral');
+    
+    menuIcon.addEventListener('click', function() {
+        menuLateral.classList.toggle('activo');
+    });
+});
