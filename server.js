40665248c31bae539fc76d8846d8ffc83215d5df
yulunez <yaru75665@gmail.com>
2025-09-12 16:40:29 -0500

    Arreglo login

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mysql = require('mysql');
+const path = require('path');
+const registroRouter = require('./routes/registro'); // Importa el router
+const cors = require('cors'); // npm install cors
 
 const app = express();
 app.use(express.json());
@@ -13,6 +16,18 @@ const db = mysql.createConnection({
     port: 3307
 });
 
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5500', 'http://127.0.0.1:5500'],
+    credentials: true
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    req.db = db;
+    next();
+});
+
 // Conectar a la base de datos MySQL
 db.connect((err) => {
     if (err) {
@@ -22,16 +37,9 @@ db.connect((err) => {
     console.log('Conectado a la base de datos MySQL');
 });
 
-// Ruta de prueba con MySQL
-app.get("/", (req, res) => {
-    db.query('SELECT NOW() as fecha', (err, results) => {
-        if (err) {
-            console.error('Error en la consulta MySQL:', err);
-            return res.status(500).send('Error en el servidor');
-        }
-        res.json(results);
-    });
-});
+
+const registroRoutes = require('./routes/registro');
+app.use('/api/registro', registroRoutes);
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en http://localhost:3000");
