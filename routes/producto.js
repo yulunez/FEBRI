19fab3810d9efafa58ea81508891d9cfac6373e4
yulunez <yaru75665@gmail.com>
2025-10-15 17:41:23 -0500

    Consulta de productos y carrusel para que juan Diego me deje tranquila

--- routes/producto.js
+++ routes/producto.js
@@ -1,8 +1,10 @@
 const express = require("express");
+
 const router = express.Router();
 const productoController = require("../control/productoContoller");
 
-router.get("/:id", productoController.obtenerProductoPorId);
+// Register explicit routes first so they aren't captured by the param route
 router.get('/top10', productoController.obtenerPrimerosDiez);
+router.get('/:id', productoController.obtenerProductoPorId);
 
 module.exports = router;
